2c3d87aea721a490f3d74a4ae9268f4ca9c12f69
apoorvas980 <78582630+apoorvas980@users.noreply.github.com>
2021-02-23 13:52:25 +0530

    Change image
    The image being used earlier had added dimensions as it was downloaded using google drive.

--- practice.py
+++ practice.py
@@ -5,7 +5,7 @@ import numpy as np
 #importing npimage
 from scipy import ndimage
 #reading the image
-brain_image=plt.imread("brain.jpg")
+brain_image=plt.imread("BrainNew.jpg")
 #print the dimensions
 print (brain_image.shape)
 #drwaing the image
