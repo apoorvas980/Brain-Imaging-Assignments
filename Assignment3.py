4968407376b59aaf3045280983feaa8771be488d
apoorvas980 <78582630+apoorvas980@users.noreply.github.com>
2021-03-21 13:22:54 +0530

    logging and timestamp

--- Assignment3.py
+++ Assignment3.py
@@ -1,7 +1,10 @@
 #importing different libraries from psychopy.
 from psychopy import visual, core, logging, event
 #to get timestamps, import datetime library
-from datetime import datetime 
+from datetime import datetime
+#to get timestamp and include it in the log file ExpLog
+timestamp=datetime.now().strftime("%d/%m%Y, %H%M%S)
+lastLog = logging.LogFile("ExpLog_"+timstamp+".log", level=logging.INFO, filemode ='w')
 #import keyboard library
 from psychopy.hardware import keyboard
 kb=keyboard.Keyboard()
